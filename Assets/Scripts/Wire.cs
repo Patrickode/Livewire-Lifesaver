3ed9f038a97cd30e15add06a56d2f0387d11d87f
Patrick <prm5983@rit.edu>
2020-04-18 17:34:44 -0400

    Added WireManager and Wire scripts, and an enum for differentiating types of wires

---
+++ Assets/Scripts/Wire.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wire : MonoBehaviour
+{
+    [HideInInspector] public Transform start;
+    [HideInInspector] public Transform end;
+    public WireType type;
+
+    private void OnEnable()
+    {
+        start = transform.GetChild(0);
+        end = transform.GetChild(transform.childCount - 1);
+
+        WireManager.wireList.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        WireManager.wireList.Remove(this);
+    }
+}
---
+++ Assets/Scripts/WireManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireManager : MonoBehaviour
+{
+    public static List<Wire> wireList = new List<Wire>();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
