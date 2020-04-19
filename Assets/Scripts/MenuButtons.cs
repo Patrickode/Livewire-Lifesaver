70e7137f7fda7a1648c91119a0c435cd797bdac6
Patrick <prm5983@rit.edu>
2020-04-19 19:26:41 -0400

    Added a main menu, an end screen, a script to make them functional, and tweaked transition speed

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public GameObject menuContainer;
+    public GameObject howToPlayContainer;
+    private bool showMenu = true;
+
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ToggleMenu()
+    {
+        if (showMenu)
+        {
+            showMenu = false;
+            menuContainer.SetActive(false);
+            howToPlayContainer.SetActive(true);
+        }
+        else
+        {
+            showMenu = true;
+            menuContainer.SetActive(true);
+            howToPlayContainer.SetActive(false);
+        }
+    }
+}
