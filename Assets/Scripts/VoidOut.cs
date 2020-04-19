1df0a3a75e63edad0863d114b2ddffd8a67d7dc0
Patrick <prm5983@rit.edu>
2020-04-19 18:19:38 -0400

    Made WireManager reload the scene on loss, and renamed PlayerDeath script to VoidOut

--- Assets/Scripts/VoidOut.cs
+++ Assets/Scripts/VoidOut.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerDeath : MonoBehaviour
+public class VoidOut : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
--- Assets/Scripts/Wire Scripts/WireManager.cs
+++ Assets/Scripts/Wire Scripts/WireManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WireManager : MonoBehaviour
 {
@@ -94,7 +95,7 @@ public class WireManager : MonoBehaviour
             else
             {
                 Destroy(current);
-                //TODO: Initiate lose sequence
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
         }
     }
