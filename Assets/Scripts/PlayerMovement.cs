f4c108f488f6f3c42d71630c55696bff7af2ed98
Patrick <prm5983@rit.edu>
2020-06-02 15:33:41 -0400

    Touched up PlayerMovement, adjusted prefab values accordingly

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,64 +15,65 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private GameObject orienter = null;
 
-    public float maxVelocity = 5;
-    public float accelSpeed = 1;
-    public float jumpPower = 5;
+    [Tooltip("The max speed the player can move without the help of external forces.")]
+    [SerializeField] private float maxMoveVelocity = 3.5f;
+    [SerializeField] private float moveSpeed = 1;
+    [SerializeField] private float jumpPower = 5;
 
     void FixedUpdate()
     {
+        //Get the direction the player's input implies and do movement logic in that direction.
+        MoveLaterally(GetInputDirection());
+
+        //If the user presses space and is touching ground, make the player jump.
+        if (Input.GetKey(KeyCode.Space) && IsGrounded())
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpPower, rb.velocity.z);
+        }
+    }
+
+    private Vector3 GetInputDirection()
+    {
         //Set up a movement vector.
-        Vector3 moveDir = Vector3.zero;
+        Vector3 inputDir = Vector3.zero;
 
         //If the WASD keys are held, add the corresponding direction to the move direction.
         if (Input.GetKey(KeyCode.W))
         {
-            moveDir += orienter.transform.forward;
+            inputDir += orienter.transform.forward;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            moveDir += -orienter.transform.right;
+            inputDir += -orienter.transform.right;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            moveDir += -orienter.transform.forward;
+            inputDir += -orienter.transform.forward;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            moveDir += orienter.transform.right;
+            inputDir += orienter.transform.right;
         }
 
-        if (Input.GetKey(KeyCode.Space) && IsGrounded())
-        {
-            ApplyJumpInput();
-        }
-
-        //Now apply that move direction to actual movement.
-        if (moveDir != Vector3.zero)
-        {
-            MoveLaterally(moveDir);
-        }
+        //Now that we've gotten all the directions of the user's input, normalize the result and return it.
+        return inputDir.normalized;
     }
 
     /// <summary>
-    /// Applies velocity in the given direction.
+    /// Applies velocity in the given direction, diregarding the y component of velocity.
     /// </summary>
-    /// <param name="forceDir">The direction to move in.</param>
-    private void MoveLaterally(Vector3 forceDir)
+    /// <param name="moveDir">The direction to move in.</param>
+    private void MoveLaterally(Vector3 moveDir)
     {
-        //Get the velocity without a y component, make a clamped speed vector, and apply it to velocity.
-        //This effectively ignores the y axis of velocity, allowing normal gravity things to happen.
-        Vector3 velSubY = new Vector3(rb.velocity.x, 0, rb.velocity.z);
-        Vector3 newVel = Vector3.ClampMagnitude(velSubY + forceDir * accelSpeed, maxVelocity);
+        //First, get the current velocity and discard the y component.
+        Vector3 velocityXZ = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+
+        //Now add movespeed to it, and then clamp it to make sure it doesn't exceed maxMoveVelocity.
+        Vector3 newVel = velocityXZ + moveDir * moveSpeed * Time.deltaTime;
+        newVel = Vector3.ClampMagnitude(newVel, maxMoveVelocity);
+
+        //Finally, now that we've set and clamped the new velocity, apply it.
         rb.velocity = new Vector3(newVel.x, rb.velocity.y, newVel.z);
-    }
-
-    /// <summary>
-    /// Makes the player jump.
-    /// </summary>
-    private void ApplyJumpInput()
-    {
-        rb.velocity = new Vector3(rb.velocity.x, jumpPower, rb.velocity.z);
     }
 
     private bool IsGrounded()
