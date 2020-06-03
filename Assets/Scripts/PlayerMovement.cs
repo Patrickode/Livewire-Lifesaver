70a772bf7ad29510e02f85645e3dde92a1d37ec4
Patrick <prm5983@rit.edu>
2020-06-03 16:39:02 -0400

    Added a multiplier to the player's gravity so their jumps feel weightier

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,24 +15,24 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private GameObject orienter = null;
 
-    [Tooltip("The max speed the player can move without the help of external forces.")]
+    [Tooltip("The max speed the player can move via input.")]
     [SerializeField] private float maxMoveVelocity = 3.5f;
     [SerializeField] private float moveSpeed = 1;
     [SerializeField] private float jumpPower = 5;
+    [SerializeField] [Range(1, 4)] private float fallGravityMultiplier = 2f;
 
-    void FixedUpdate()
+    private void Start()
     {
-        //Get the direction the player's input implies and do movement logic in that direction.
-        MoveLaterally(GetInputDirection());
-
-        //If the user presses space and is touching ground, make the player jump.
-        if (Input.GetKey(KeyCode.Space) && IsGrounded())
+        //We subtract jumpGravityMultiplier by one because Physics.gravity.y is already being added
+        //once every frame; by doing this, we ensure the "multiplier" is accurate in the jump code
+        //We don't want to multiply by zero, though, so only subtract if greater than 1
+        if (fallGravityMultiplier > 1)
         {
-            rb.velocity = new Vector3(rb.velocity.x, jumpPower, rb.velocity.z);
+            fallGravityMultiplier -= 1;
         }
     }
 
-    private Vector3 GetInputDirection()
+    void FixedUpdate()
     {
         //Set up a movement vector.
         Vector3 inputDir = Vector3.zero;
@@ -55,8 +55,30 @@ public class PlayerMovement : MonoBehaviour
             inputDir += orienter.transform.right;
         }
 
-        //Now that we've gotten all the directions of the user's input, normalize the result and return it.
-        return inputDir.normalized;
+        //Now that we've gotten all the directions of the user's input, normalize it and move with it.
+        MoveLaterally(inputDir.normalized);
+    }
+
+    private void Update()
+    {
+        //If the user presses space and is touching ground, make the player jump.
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpPower, rb.velocity.z);
+        }
+
+        //If the player isn't holding space after they jump, and they haven't hit the peak of their jump yet,
+        //Increase gravity to allow for a short hop
+        if (!Input.GetKey(KeyCode.Space) && rb.velocity.y > 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * fallGravityMultiplier * Time.deltaTime;
+        }
+        //Otherwise, if the player has reached the peak of their jump, increase gravity to make the jump feel
+        //weightier
+        else if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * fallGravityMultiplier * Time.deltaTime;
+        }
     }
 
     /// <summary>
@@ -78,7 +100,9 @@ public class PlayerMovement : MonoBehaviour
 
     private bool IsGrounded()
     {
-        //Thanks to http://answers.unity.com/answers/196395/view.html for this!
-        return Physics.Raycast(transform.position, -Vector3.up, coll.bounds.extents.y + 0.05f);
+        //Inspired / adapted from http://answers.unity.com/answers/196395/view.html
+        //Cast a sphere, very slightly smaller than the player, downward a by a very small amount. This checks if
+        //there is a collider right below the player.
+        return Physics.SphereCast(transform.position, coll.bounds.extents.y - 0.0001f, Vector3.down, out _, 0.002f);
     }
 }
