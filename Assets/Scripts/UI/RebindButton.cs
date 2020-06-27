c8ab34bba928ad70d618eac8e9b1de2b43310b00
Patrick <prm5983@rit.edu>
2020-06-27 14:27:34 -0400

    Tweaked Options menu button and attempted to dynamically fill control with button in use; failed

---
+++ Assets/Scripts/UI/RebindButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class RebindButton : MonoBehaviour
+{
+    //[SerializeField] private Button button = null;
+    //[SerializeField] private TextMeshProUGUI buttonText = null;
+    //[SerializeField] private InputActionReference actionRef = null;
+    //private InputAction action = null;
+
+    //private void Start()
+    //{
+    //    action = actionRef.action;
+    //    buttonText.text = InputControlPath.ToHumanReadableString
+    //        (
+    //        action.bindings[0].effectivePath,
+    //        InputControlPath.HumanReadableStringOptions.OmitDevice
+    //        );
+    //}
+}
