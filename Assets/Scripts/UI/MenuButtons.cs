ad872d6166800f1b571f1c6c466e600acb8d7f7c
Patrick <prm5983@rit.edu>
2020-07-06 14:40:56 -0400

    Updated GetCurrentMenu to account for non-immediate children

--- Assets/Scripts/UI/MenuButtons.cs
+++ Assets/Scripts/UI/MenuButtons.cs
@@ -24,22 +24,41 @@ public class MenuButtons : MonoBehaviour
     /// Also sets defaultMenu the first time through.
     /// This will only happen once.
     /// </summary>
-    private void GetCurrentMenu()
+    /// <param name="transformToCheck">The transform to look in. Defaults to this script's transform.</param>
+    /// <returns>Whether currentMenu was assigned or not.</returns>
+    private bool GetCurrentMenu(Transform transformToCheck = null)
     {
+        //If transformToCheck is null, use this script's transform instead. Otherwise, just use transformToCheck.
+        transformToCheck = transformToCheck ? transformToCheck : transform;
+
         //Only get the current menu if currentMenu is uninitialized / null.
         if (!currentMenu)
         {
-            //Get all children of the canvas, and set currentMenu equal to the first active menu.
-            foreach (Transform child in transform)
+            //For each child of this object,
+            foreach (Transform child in transformToCheck)
             {
-                if (child.gameObject.activeInHierarchy && child.CompareTag("Menu"))
+                //Check if its active. (We don't need to check its children, because they'll be inactive if this
+                //is inactive.)
+                if (child.gameObject.activeInHierarchy)
                 {
-                    currentMenu = child.gameObject;
-                    if (!defaultMenu) { defaultMenu = child.gameObject; }
-                    return;
+                    //If it is, and it's a menu, we found the first active menu. Set currentMenu equal to it.
+                    if (child.CompareTag("Menu"))
+                    {
+                        currentMenu = child.gameObject;
+                        if (!defaultMenu) { defaultMenu = child.gameObject; }
+                        return true;
+                    }
+                    //If it's not a menu, it's a container; check all of its children, and if we find
+                    //a menu when we do, return true.
+                    else
+                    {
+                        if (GetCurrentMenu(child)) { return true; }
+                    }
                 }
             }
         }
+
+        return false;
     }
 
     private void Start() { GetCurrentMenu(); }
