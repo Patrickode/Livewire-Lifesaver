d9b326922ca1e4c0b0ea2d2e7cf9603d257ab87d
Patrick <prm5983@rit.edu>
2020-07-31 14:28:42 -0400

    Made golt bolts "collectable"

---
+++ Assets/Scripts/BoltCollect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoltCollect : MonoBehaviour
+{
+    [SerializeField] GameObject collectParticleObj = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Collected();
+        }
+    }
+
+    private void Collected()
+    {
+        if (collectParticleObj) { Instantiate(collectParticleObj, transform.position, Quaternion.identity); }
+        gameObject.SetActive(false);
+    }
+}
