bbb72d27441477b53034efefc43241802daf30a1
Patrick <prm5983@rit.edu>
2020-08-17 12:02:57 -0400

    Added code to UnlockLevel that shows whether a bolt has been collected at that level

--- Assets/Scripts/UI/UnlockLevel.cs
+++ Assets/Scripts/UI/UnlockLevel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 #if UNITY_EDITOR
 using UnityEngine.InputSystem;
 #endif
@@ -9,14 +10,47 @@ using UnityEngine.InputSystem;
 public class UnlockLevel : MonoBehaviour
 {
     [SerializeField] private Button button = null;
+    [SerializeField] private RawImage boltIcon = null;
+    [SerializeField] private Texture emptyIcon = null;
+    [SerializeField] private Texture filledIcon = null;
     [Space(10)]
     [SerializeField] private int index = -1;
+    [Space(10)]
+    [SerializeField] private bool levelHasBolt = false;
+
+#if UNITY_EDITOR
+    [SerializeField] private bool toggleBoltIcon = false;
+    private void OnValidate()
+    {
+        if (toggleBoltIcon && boltIcon)
+        {
+            boltIcon.gameObject.SetActive(!boltIcon.gameObject.activeSelf);
+            toggleBoltIcon = false;
+        }
+    }
+#endif
 
     void Start()
     {
+        //If the level doesn't have a bolt to collect, deactivate the bolt icon.
+        boltIcon.gameObject.SetActive(levelHasBolt);
+
+        //If the player hasn't gotten to this level yet, make the button unclickable and deactivate the bolt icon.
         if (index > SaveManager.CachedData.LastCompletedIndex + 1)
         {
             button.interactable = false;
+            boltIcon.gameObject.SetActive(false);
+        }
+
+        //If the player has collected the bolt in this level, make the bolt icon filled in.
+        //Otherwise, make it empty.
+        else if (levelHasBolt && SaveManager.CachedData.CollectedBoltIndices.Contains(index))
+        {
+            boltIcon.texture = filledIcon;
+        }
+        else
+        {
+            boltIcon.texture = emptyIcon;
         }
     }
 
