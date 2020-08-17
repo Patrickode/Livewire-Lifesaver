7cc2847aaf77228aeba486166c68c9a822650952
Patrick <prm5983@rit.edu>
2020-08-17 13:05:46 -0400

    Added code to automatically add a corner cap to a wire with one click

--- Assets/Scripts/Wire & Current/Wire.cs
+++ Assets/Scripts/Wire & Current/Wire.cs
@@ -10,6 +10,36 @@ public class Wire : MonoBehaviour
     public WireType type;
     [HideInInspector] public bool playerClose;
 
+#if UNITY_EDITOR
+    [Header("Auto-Add Corner Cap")]
+    [SerializeField] private GameObject cornerCap = null;
+    [SerializeField] private bool addCornerCap = false;
+
+    //This is used to circumvent enigmatic, irrelevant warnings in the console about SendMessage,
+    //which is clearly never used in this segment.
+    //Gotten from https://forum.unity.com/threads/sendmessage-cannot-be-called-during-awake-checkconsistency-or-onvalidate-can-we-suppress.537265/#post-5560519
+    void OnValidate() { UnityEditor.EditorApplication.delayCall += _OnValidate; }
+    private void _OnValidate()
+    {
+        if (this == null) { return; }
+
+        if (cornerCap && addCornerCap)
+        {
+            //Instantiate a corner cap and set it to the right size / position
+            GameObject addedCap = Instantiate(cornerCap, gameObject.transform);
+            addedCap.transform.localScale = new Vector3
+            (
+                addedCap.transform.localScale.x / transform.localScale.x,
+                addedCap.transform.localScale.y / transform.localScale.y,
+                addedCap.transform.localScale.z / transform.localScale.z
+            );
+            addedCap.transform.localPosition = new Vector3(0, 0, 0.5f);
+
+            addCornerCap = false;
+        }
+    }
+#endif
+
     private void OnEnable()
     {
         WireManager.AddWire(this);
